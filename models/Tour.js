ef29c3ca81b12264e642ab89a4212bb5ed731c76
Adeeb Ogaili <adeebnasser@gmail.com>
2023-04-14 14:25:12 -0500

    fixed user model

--- models/Tour.js
+++ models/Tour.js
@@ -1,29 +1,60 @@
 const mongoose = require('mongoose');
 
 // Define the schema for the user model
-const userSchema = new mongoose.Schema(
+const tourSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
+    title: {
       type: String,
       required: true,
       unique: true,
     },
-    password: {
+    city: {
       type: String,
       required: true,
     },
-    isAdmin: {
-      type: Boolean,
+    address: {
+      type: String,
       required: true,
+    },
+    distance: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: true,
+    },
+
+    reviews: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+
+    featured: {
+      type: Boolean,
       default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
   { timestamps: true }
 );
 
-// Create the user model from the schema and export it
-module.exports = mongoose.model('User', userSchema);
+// Create the Tour model from the schema and export it
+module.exports = mongoose.model('Tour', tourSchema);
